9507c4a744fb65eb27f54925104b4cf4cc189c75
tsabelmann <tim.sabelmann@hotmail.de>
2020-11-15 14:08:41 +0100

    feat: Changed main.py to rely on eventlogger.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 # Import E
-from eventlogger.ui.window import MainWindow
-
-# Import P
-from PySide2 import QtWidgets
-
-# Import S
-import sys
+from eventlogger.eventlogger as eventlogger
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    ui = MainWindow()
-    app.exec_()
+    eventlogger.main()
